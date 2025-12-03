10af57c63383b3dbaa4a947182adf150a05e440e
Thiago Debia <thiagodebia@gmail.com>
2025-12-03 20:37:15 -0300

    Conectando no SQL com PyODBC

---
+++ app.py
@@ -0,0 +1,28 @@
+#docker run -dti --name python-sql -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Senha@123" -e "MSSQL_PID=Express" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+#docker exec -it 3fa10032 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P senha123
+import pyodbc
+
+print(pyodbc.drivers())
+
+
+# Se conecta com o banco
+cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=master;UID=sa;PWD=Senha@123')
+
+# Cria um cursor
+cursor = cnxn.cursor()
+
+# Lista todas as tabelas (usuário + sistema)
+cursor.execute("""
+    SELECT TABLE_SCHEMA, TABLE_NAME
+    FROM INFORMATION_SCHEMA.TABLES
+    ORDER BY TABLE_SCHEMA, TABLE_NAME
+""")
+
+tables = cursor.fetchall()
+
+if not tables:
+    print("Nenhuma tabela encontrada no banco.")
+else:
+    print("Tabelas encontradas:")
+    for schema, table in tables:
+        print(f"{schema}.{table}")
