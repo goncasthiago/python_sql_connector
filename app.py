7353ff33abc30a9ecc00972128a983ab4072b7a3
Thiago Debia <thiagodebia@gmail.com>
2025-12-03 21:24:51 -0300

    Inserindo, atualizando e apagando registros no Banco SQL com Python

--- app.py
+++ app.py
@@ -28,13 +28,48 @@ else:
         print(f"{schema}.{table}")
 
 # Criando uma tabela
-cursor.execute('CREATE TABLE clientes (' \
-'id int IDENTITY(1,1) PRIMARY KEY , ' \
-'nome VARCHAR(100), ' \
-'email VARCHAR(150),' \
-'CreatedAt DATE DEFAULT CURRENT_TIMESTAMP NOT NULL'\
-')')
+def criar_tabela(conexao, cursor, tabela):
+    cursor.execute(f"CREATE TABLE {tabela} (" \
+    'id int IDENTITY(1,1) PRIMARY KEY , ' \
+    'nome VARCHAR(100), ' \
+    'email VARCHAR(150),' \
+    'CreatedAt DATE DEFAULT CURRENT_TIMESTAMP NOT NULL'\
+    ')')
+    # Comitar para 'salvar' o que foi feito
+    conexao.commit()
 
-#cursor.execute('INSERT INTO clientes (id, nome, email) VALUES (1, "Thiago","thiagodebia@gmail.com")')
+#criar_tabela(cnxn, cursor, 'clientes')
 
-#cursor.execute('SELECT * FROM clientes')
+#data = ('Thiago', 'thiagodebia@gmail.com')
+
+def inserir_registro(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute('INSERT INTO clientes (nome, email) VALUES (?,?)', data)
+    conexao.commit()
+
+def atualizar_registro(conexao, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute('UPDATE clientes SET nome=?, email=? WHERE id=?;', data)
+    conexao.commit()
+
+#atualizar_registro(cnxn, cursor, "Thiago Debia", 'thiago@hotmail.com', 1)
+
+def deletar_registro(conexao, cursor, id):
+    data = (id,)
+    cursor.execute('DELETE FROM clientes WHERE id=?;', data)
+    conexao.commit()
+
+#deletar_registro(cnxn, cursor,1)
+
+def inserir_muitos(conexao, cursor, dados):
+    cursor.executemany('INSERT INTO clientes (nome, email) VALUES (?,?)', dados)
+    conexao.commit()
+
+dados=[
+    ('Thiago','thiago@gmail.com'),
+    ('Juliana','juliana@gmail.com'),
+    ('Arthur','arthur@gmail.com')
+]
+
+#inserir_muitos(cnxn, cursor, dados)
+    
